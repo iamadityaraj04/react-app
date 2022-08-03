e8766a282beb210cd67cfa7442dc71827358b67a
iamadityaraj04 <adityarohitraj@gmail.com>
2022-08-03 12:47:18 +0530

    react

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 
+const name="Aditya Raj";
+const regNo=11911045;
+const date=new Date();
+const currDate=date.getDate();
+const currMonth=date.getMonth();
+const currYear=date.getFullYear();
+
 function App() {
   return (
     <div className="App">
+    <div className="info">
+      <span>Date: {currDate} </span><span>/ {currMonth}/ </span><span>{currYear}</span>
+      <p>Name : {name} </p>
+      <p>RegNo. : {regNo}</p>
+    </div>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
