813ead5533c426a400f085cd19731d1434be7e12
iamadityaraj04 <adityarohitraj@gmail.com>
2022-08-05 14:00:51 +0530

    react

--- src/App.js
+++ src/App.js
@@ -4,18 +4,30 @@ import './App.css';
 const name="Aditya Raj";
 const regNo=11911045;
 const date=new Date();
-const currDate=date.getDate();
-const currMonth=date.getMonth();
-const currYear=date.getFullYear();
+const currDate=date.toLocaleDateString();
+var currTime=date.toLocaleTimeString();
+
+//styling time
+const customStyle={
+  color:"",
+}
+if(date.getHours()<12){
+  customStyle.color="green";
+}else if(date.getHours()<18){
+  customStyle.color="orange";
+}else{
+  customStyle.color="darkgrey";
+}
 
 function App() {
   return (
     <div className="App">
-    <div className="info">
-      <span>Date: {currDate} </span><span>/ {currMonth}/ </span><span>{currYear}</span>
+
+      {/* inline styling is done by creating javascript object */}
+      <h1 style={{color: "red"}}>Date: {currDate}</h1>
+      <h1 style={customStyle}>Time: {currTime}</h1>
       <p>Name : {name} </p>
       <p>RegNo. : {regNo}</p>
-    </div>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
