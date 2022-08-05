46a1442b0f59912fe7f90b4c3dc642dec6b9eaf2
iamadityaraj04 <adityarohitraj@gmail.com>
2022-08-05 18:21:27 +0530

    react component

---
+++ src/Heading.js
@@ -0,0 +1,7 @@
+// creating custom component
+
+function Heading() {
+    return <h1>This is a custom heading component.</h1>
+}
+
+export default Heading;
--- src/index.js
+++ src/index.js
@@ -2,12 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+
+// importing custom heading component
+import Heading from './Heading';
+
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+
+    {/* custom react component i.e App */}
+    <Heading />
     <App />
+
   </React.StrictMode>
 );
 
